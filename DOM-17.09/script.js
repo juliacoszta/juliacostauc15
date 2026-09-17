a67ace0e29a77af4a9ffde2ec44a4eb8792c4390
juliacoszta <aj3386833@gmail.com>
2026-09-17 10:27:59 -0300

    Add files via upload

---
+++ DOM-17.09/script.js
@@ -0,0 +1,9 @@
+   function mudartexto () {
+      document.getElementById
+        ("titulo").textContent ="voce clicou no botao!";
+}
+    
+    function mudarcor () {
+      document.getElementById("mensagem").style.color =
+       "purple";
+    }
